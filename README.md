4c14ca4af98066cbc22f16a598b28a60db3cacc1
Songcheng <sawasekiri@gmail.com>
2018-10-27 20:31:00 +0100

    added ascii_confess.py and its description.

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 This is a growing collection of my small programmes.
 
 ## Contents
-- word_practice
+- word_practice: a typing practice programme, inspired by a series of wrong typing when I type the password.
+- ascii_confess: a ascii converting programme, used to create a string of asciis, that contains information. I personally used this for love message encryption:- not too hard to decipher, and looks adequately neat.
 
---
+++ ascii_confess.py
@@ -0,0 +1,53 @@
+
+def convert_ascii(text, manual_return = False):
+    text_ascii = ''
+
+    for i in text:
+        for j in i:
+            if manual_return:
+                print('{}({})'.format(text_ascii, j), end='')
+                put_return = bool(input())
+                if put_return:
+                    text_ascii += '\n'
+                    text_ascii += str(ord(j))
+                else:
+                    text_ascii += str(ord(j))
+                    text_ascii += ' '
+            else:
+                text_ascii += str(ord(j))
+                text_ascii += ' '
+        text_ascii += ' '
+
+    return(text_ascii)
+
+def select_mode(menu):
+    while True:
+        try:
+            print('Please select from the following mode: ')
+            for k, v in menu.items():
+                print('{}: {}'.format(v, k))
+            choice = int(input(''))
+            if choice not in menu:
+                raise ValueError
+            else:
+                break
+        except ValueError:
+            print('Invalid entry. Please try again.')
+    return choice
+
+if __name__ == "__main__":
+    menu = {1: 'output as a line',
+            2: 'output with returns manually',}
+
+    text = str(input('Please enter your message: '))
+    text_l = text.strip().split()  # obtain a list containing words
+    mode = select_mode(menu)  # select mode from menu
+
+    if mode == 1:
+        print(convert_ascii(text_l))
+
+    elif mode == 2:
+        print('''Press return to skip the current letter;
+                enter any words to add a return after the current letter.''')
+        print(convert_ascii(text_l, manual_return = True))
+
