c8387844d7bc328a2d1ad5775965052eaacf80d4
LiSongcheng <sawasekiri@gmail.com>
2019-01-01 00:52:43 +0000

    Add new features
    1. the progamme now takes system argument, which makes it compatible with Alfred Workflows
    2. slightly better format and code style
    3. ready for the analysis function

--- word_practice.py
+++ word_practice.py
@@ -1,45 +1,64 @@
-import time
 import sys
+import time
+
 
 def check(wrd, inp):
+    '''
+    check if user's input agree with the word.
+    '''
     if inp == wrd:
         return True
-    else:
-        return False
+
+
+def analysis(failure: list, n: int):
+    # TODO: use the Levenshtein Algorithm, since they may differ in length as
+    # well (Hamming distance not applicable)
+    print('Feature not completed so far.')
 
 
 if __name__ == "__main__":
     print('Welcome to this typing exercise.')
+
+    ask_input = False
     try:
         word = sys.argv[1]
         print('The text going to practice is {}'.format(word))
     except IndexError or NameError:
+        # No sys argv, then ask for input
         ask_input = True
 
     while True:
         try:
             if ask_input:
-                word = str(input('Please type in the word you\'d like to practice: '))
+                word = str(
+                    input('Please type in the word you\'d like to practice: ')
+                )
                 print('The text going to practice is {}'.format(word))
 
+            # Ask for times to practice
             n = int(input('the times of practice: '))
-            f = 0  # failure
-
             if n <= 0:
                 raise ValueError
 
+            invalid_entries = []
             t_0 = time.time()  # count start time
 
             for i in range(0, n):
                 entry = str(input('entry: '))
                 if not check(word, entry):
-                    f += 1
+                    invalid_entries.append(entry)
                     print('ooops!')
 
             t = round(time.time() - t_0, 2)
-            accu = round((1 - f / n) * 100, 1)  # accuracy
+            failure = len(invalid_entries)
+            accuracy = round((1 - failure / n) * 100, 1)  # accuracy
 
-            report =  {'Number of trial ': n, 'Failure ': f, 'Accuracy': accu, 'Time used ': t}
+            report = {
+                'Number of trial ': n,
+                'Failure ': failure,
+                'Accuracy': accuracy,
+                'Time used ': t,
+            }
 
             print('')
             break
@@ -47,9 +66,17 @@ if __name__ == "__main__":
         except ValueError:
             print('Invalid entry, please try again.')
 
+    # Print statistics
+    print('...\nStatistics')
     for k, v in report.items():
         print('{}: {}'.format(k, v))
 
+    # Ask if requires analysis
+    ask_further = not bool(
+        input('...\nReturn to see further analysis, other keys to exit.')
+    )
+    if ask_further:
+        analysis(invalid_entries, n)
+
     print('\ndone!')
     exit()
-
