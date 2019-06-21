a28019571340c35f167c18076a85716a62eb50c0
BaksiLi <26314680+BaksiLi@users.noreply.github.com>
2019-06-21 17:04:57 +0800

    Update ewformatter
    to ver. 0.0.1.

--- src/eastern-western_formatter.py
+++ src/eastern-western_formatter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+__version__ = '0.0.1'
 # This scritp adds spaces to eastern(Chinese, Japanese, Korean [^1])-western(Latin, Cyrillic, Greek), according to the [guidelines](https://github.com/sparanoid/chinese-copywriting-guidelines).
 # [1]: https://en.wikipedia.org/wiki/List_of_Unicode_characters#East_Asian_writing_systems
 
@@ -13,11 +14,11 @@ Western = {
     'LatinUpper': (65, 90),
     'LatinLower': (97, 122),
     'LatinSupp': (192, 255)
-}  # except 215, 247
-
-NoSpaces = [i for i in '，。；「」：《》『』、[]（）*_']
-
+}  # should exclude 215, 247
+# test_western = ['a', 'ŭ', 'g', ',', 'β']
 # Latin Extended-A & -B, Esperanto ŭ et al in this blocks
+
+NoSpaces = '，。；「」：《》『』、[]（）*_'
 
 
 def is_western(char: str) -> bool:
@@ -25,47 +26,63 @@ def is_western(char: str) -> bool:
     Determine if char belongs to the western codes.
     '''
     codes = []
-    for code_ranges in Western.values():
-        for code in range(code_ranges[0], code_ranges[1]):
-            codes.append(code)
+    for i, j in Western.values():
+        codes += (list(range(i, j + 1)))
     return ord(char) in codes
-# test_western = ['a', 'ŭ', 'g', ',', 'β']
+
+
 # for i in lis:
 #    print(is_western(i))
 
 
-def is_not_fullwidth(char: str) -> bool:
+def is_nonspacing_class(char: str) -> bool:
     '''
     Determine if char belongs to the fullwidth codes.
     '''
     return not char.isspace() and not (char in NoSpaces)
 
-def apply_spaces(pre, ego):
+
+def apply_rules(pre, post):
     '''
+    Apply (given local) rule to the pre- and post- elements given by functools.reduce from a string.
     '''
+    # check validity
     if len(pre) == 0:
-        return ego
+        return post
 
-    if is_western(pre[-1]) != is_western(ego) and \
-            is_not_fullwidth(ego) and is_not_fullwidth(pre[-1]):
-        return ' '.join([pre, ego])
+    # set rule
+    # explained: if the pre-word is ended in a western char, while the post-word is not in accordance with the pre-word, then apply space.
+    rule = is_western(pre[-1]) != is_western(post) and \
+            is_nonspacing_class(post) and is_nonspacing_class(pre[-1])
+
+    if rule:
+        return ' '.join([pre, post])
     else:
-        return ''.join([pre, ego])
+        return ''.join([pre, post])
 
-# TODO: auto-spacing between quantities and their units, e.g. `20TB` -> `20 TB`.
+
+# TODO: Greek and Cyrillic.
+# TODO: Kana and Hangul (optional), should provide options.
+# TODO: Maths, e.g. `數學式為f:=x+1` -> `數學式為 f := x + 1`. So `:` and `=` should belongs to the same class from fullwidth.
+# TODO: auto-spacing between quantities and their units, e.g. `20TB` -> `20 TB`, with exception `°`.
+# TODO: strip unwanted spaces (anti-rule char spacings)
+# TODO: Better module managemen.
 
 if __name__ == '__main__':
     # check for valid argument
     if len(sys.argv) < 2:
         print('At least one argument is required!')
         exit()
-    
+
     # read from the given file
     with open(sys.argv[1], 'r') as f:
         input_str = f.read()
-    
+
     # TODO: take options of new file
+    # TODO: take options of kana, hangul (later)
     # write the result
-    output_str = functools.reduce(apply_spaces, input_str, '')
+    output_str = functools.reduce(apply_rules, input_str, '')
     with open(sys.argv[2 if len(sys.argv) > 2 else 1], 'w') as f:
         f.write(output_str)
+
+    # test = 'A紐幣的BAa as和s，沒想到 「a」無用'
